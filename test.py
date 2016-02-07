3e12ec33e4fdc5b6680c801bbbf7cbf937ad8ec2
Edward M. Meshuris <edward@theemm.com>
2016-02-07 01:33:29 -0800

    .

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ GPIO.setmode(GPIO.BOARD)
 #setup pin 18 as an output
 GPIO.setup(18,GPIO.OUT)
 #lights off
-GPIO.output(18,GPIO.HIGH)
+#GPIO.output(18,GPIO.HIGH)
 
 #lights on
 GPIO.output(18,GPIO.LOW)
